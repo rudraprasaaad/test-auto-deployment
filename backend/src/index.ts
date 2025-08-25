5abb743d3369beb3cd1d8a6ea6e058966d66e233
rudraprasaaad <goudarudra23@gmail.com>
2025-08-25 14:58:00 +0530

    moved to src folder

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const body = req.body || "Hello!!";
+  res.status(201).json({
+    msg: body,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started at " + 3000);
+});
